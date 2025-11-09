d193bd9856e5129de953735aa306762597c933f5
Vinicius Bender <viniciusbender01@outlook.com>
2025-11-08 23:25:31 -0300

    add scripts `lint:check` and `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
-function Home (){
-
-    return (
-        <>
-        <h1>I'm gonna make it!</h1>
-        </>
-    );
+function Home() {
+  return (
+    <>
+      <h1>I'm gonna make it!</h1>
+    </>
+  );
 }
 
 export default Home;
